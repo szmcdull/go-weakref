db35151706924545752048b30286b59646d7f550
szmcdull <machine.gw@gmail.com>
2022-09-11 22:42:07 +0800

    race test case and the fix

--- weakref.go
+++ weakref.go
@@ -13,6 +13,9 @@ type (
 	}
 )
 
+// Caution should be taken when v is an address taken of an item from a slice/map.
+// When the slice/map grows the item would be copied to a new address,
+// the original one is freed, and IsAlive() will return false.
 func NewWeakRef[T any](v *T) *WeakRef[T] {
 	result := &WeakRef[T]{
 		p: uintptr(unsafe.Pointer(v)),
@@ -32,8 +35,23 @@ func IsAlive[T any](r *WeakRef[T]) bool {
 	return r.p != 0
 }
 
-func Get[T any](r *WeakRef[T]) *T {
+func Get[T any](r *WeakRef[T]) (result *T) {
 	r.Lock()
 	defer r.Unlock()
-	return (*T)(unsafe.Pointer(r.p))
+	defer func() {
+		if e := recover(); e != nil { // finalizer not called yet, but invalid pointer detected
+			r.p = 0
+			result = nil
+		}
+	}()
+
+	// currently Go does not move an variable if it is already in the heap.
+	// but no guaranties in the future.
+	result = (*T)(unsafe.Pointer(r.p))
+
+	// when the finalizer is not called soon enough, invalid pointer may be used.
+	// test if the pointer is still valid
+	_ = *result
+
+	return result
 }
--- weakref_test.go
+++ weakref_test.go
@@ -2,8 +2,14 @@ package weakref
 
 import (
 	"runtime"
+	"sync"
 	"testing"
 	"time"
+)
+
+var (
+	wg          sync.WaitGroup
+	testingRace bool
 )
 
 func makeRef() *WeakRef[int] {
@@ -13,19 +19,74 @@ func makeRef() *WeakRef[int] {
 	return r
 }
 
-func TestNewWeakRef(t *testing.T) {
-	r := makeRef()
+func testNewWeakRef(i int, t *testing.T) {
+	//r := makeRef()
+	a := 123
+	p := &a
+	r := NewWeakRef(p)
+
 	if !IsAlive(r) {
 		t.Error(`early freed`)
 	}
 	if *Get(r) != 123 {
 		t.Fail()
 	}
+	_ = &a
 	runtime.GC()
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(time.Millisecond * 1)
 	runtime.GC()
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(time.Millisecond * 1)
+	if IsAlive(r) {
+		if !testingRace {
+			t.Error(`not freed`)
+		}
+	}
+
+	wg.Done()
+}
+
+func testNewFromSlice(i int, t *testing.T) {
+	a := []int{123, 222, 333}
+	r := NewWeakRef(&a[0])
+	if !IsAlive(r) {
+		t.Error(`early freed`)
+	}
+	if *Get(r) != 123 {
+		t.Fail()
+	}
+
+	a = append(a, make([]int, 256)...)
+	runtime.GC()
+	time.Sleep(time.Millisecond * 1)
+	runtime.GC()
+	time.Sleep(time.Millisecond * 1)
 	if IsAlive(r) {
 		t.Error(`not freed`)
 	}
+	func() {
+		defer func() {
+			e := recover()
+			if e == nil {
+				t.Error(`slice not moved`)
+			}
+		}()
+		if *Get(r) == a[0] {
+			t.Error(`slice not moved`)
+		}
+	}()
+
+	wg.Done()
+}
+
+func TestRace(t *testing.T) {
+	testingRace = true
+	testCount := 10000
+	wg.Add(testCount * 2)
+	for i := 0; i < testCount; i++ {
+		ii := i
+		go testNewWeakRef(ii, t)
+		go testNewFromSlice(ii, t)
+	}
+	wg.Wait()
+	testingRace = false
 }
